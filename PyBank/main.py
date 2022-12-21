62b5b420b360af75db299b4e6975ec69757cafd0
clangstonhinton <clangstonhinton@gmail.com>
2022-12-21 18:03:10 -0500

    uploading HW

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,6 @@ import os
 # Import module csv for reading CSV files
 import csv
 
-# Import statistics module
-import statistics
-
 # Point to path of csvfile
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
@@ -19,70 +16,71 @@ with open(csvpath) as csvfile:
     # Read the header row first 
     csv_header = next(csvreader)
 
-    # Create variables to store list data 
-    count_months = []
+    # Create variables to store data
+    count_months = 0
     total_prof = []
     monthly_change = []
+    date = []
 
     for row in csvreader:
         # Add up the total number of months in the dataset
-        count_months.append(row[0])
+        count_months = count_months + 1
+        
         # Add up the net total amount of "Profit/Loss" over the period
         total_prof.append(int(row[1]))
-        # Calculate the change from the prior month (current row +1) - (current row)
-    
+
+        # Calculate the change from the prior month (current row) - (previous row)
         x = (len(total_prof)-1)
         monthly_change.append(total_prof[x]-total_prof[x-1])
 
+        # Append the date
+        date.append(row[0])
+
 # Find the greatest increase in profits (date and amount) over the period
-# This will be the MAX value of the output of Row 13 in code above
+# This will be the MAX value of the output of monthly_change code above
 max_inc = max(monthly_change)
 
 # Find the greatest decrease i profits (date and amount) over the period
-# This will be the MIN value of the output of Row 13 in code above
+# This will be the MIN value of the output of monthly_change code above
 max_dec = min(monthly_change)
 
-x = len(monthly_change)
-
+max_inc_date = date[monthly_change.index(max_inc)]
+max_dec_date = date[monthly_change.index(max_dec)]
 
 # Print the following in the terminal
-# Write title "Financial Analysis"
+# Print title "Financial Analysis"
 print("Financial Analysis")
 
-# Write "-----------------------" separator
+# Print "-----------------------" separator
 print("-----------------------------")
 
-# Write Total Months, Total P/L, Average Change, Greatest Inc (date/amt), Greated Dec (data/amt)
-print(f'Total Months: {len(count_months)}')
+# Pring Total Months, Total P/L, Average Change, Greatest Inc (date/amt), Greated Dec (data/amt)
+print(f'Total Months: {(count_months)}')
 print(f'Total: ${int(sum(total_prof))}')
-print(f'Total Monthly Change: ${int(sum(monthly_change))}')
-print(f'Average Change: ${int(sum(monthly_change)/len(monthly_change))}')
-print(f'Greatest Increase in Profits:  ${max(monthly_change)}')
-print(f'Greatest Decrease in Profit: ${min(monthly_change)}')
+print(f'Average Change: ${sum(monthly_change)/((count_months)-1):.2f}')
+print(f'Greatest Increase in Profits:  {max_inc_date} (${max(monthly_change)})')
+print(f'Greatest Decrease in Profit: {max_dec_date} (${min(monthly_change)})')
         
-
 # Write the analysis results to a text file
 # Set variable for the output file
 pybank_output = os.path.join("Analysis", "pybank_analysis.txt")
 
 # Open the output file
-#with open(pybank_output, "w") as datafile:
+with open(pybank_output, "w") as datafile:
 
     # Write the following to the output text file
     # Write title "Financial Analysis"
-    #datafile.write("Financial Analysis")
+    datafile.write("Financial Analysis")
 
     # Write "-----------------------" separator
-    #datafile.write("\n-----------------------------")
+    datafile.write("\n-----------------------------\n")
 
     # Write Total Months, Total P/L, Average Change, Greatest Inc (date/amt), Greated Dec (data/amt)
-    #datafile.write(f'\nTotal Months: {len(count_months)}')
-    #datafile.write(f'\nTotal: ${int(sum(total_prof))}')
-    #datafile.write(f'\nAverage Change: ${int(sum(monthly_change)/len(monthly_change -1),2)}')
-    #datafile.write(f'\nGreatest Increase in Profits: ${max(monthly_change)}')
-    #datafile.write(f'\nGreatest Decrease in Profit: ${min(monthly_change)}')
+    datafile.write(f'Total Months: {(count_months)}\n')
+    datafile.write(f'Total: ${int(sum(total_prof))}\n')
+    datafile.write(f'Average Change: ${sum(monthly_change)/((count_months)-1):.2f}\n')
+    datafile.write(f'Greatest Increase in Profits:  {max_inc_date} (${max(monthly_change)})\n')
+    datafile.write(f'Greatest Decrease in Profit: {max_dec_date} (${min(monthly_change)})\n')
 
-#Calcuate the average monthly change and round it    
-    #av_mon_chng = round(sum(changes)/(month_count -1),2)
-    
+
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,6 @@ import os
 # Import module csv for reading CSV files
 import csv
 
-
 # Point to path of csvfile
 csvpath = os.path.join('Resources', 'election_data.csv')
 
@@ -12,75 +11,72 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath) as csvfile:
 
     # CSV reader specifices delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.DictReader(csvfile)
 
-    # Read the header row first 
-    csv_header = next(csvreader)
-
-    # Create variables to store list data 
-    total_votes_cast = []
-    candidates_list_dup = []
-    candidates_unique = []
+    # Create variables to store data
+    total_votes = 0
+    candidate_names = {}
+    max_votes = 0
+    winner = ""
 
     for row in csvreader:
-        # Add up the total number of votes cast
-        total_votes_cast.append(row[0])
-        # Add candidate voted for to candidate list
-        candidates_list_dup.append(row[2])
-
-    for item in candidates_list_dup:
-        if item not in candidates_unique:
-            candidates_unique.append(item)
-
+        # Add up the total number of months in the dataset
+        if not row['Candidate'] in candidate_names:
+            candidate_names[row['Candidate']] = 1
+        else:
+            candidate_names[row['Candidate']] += 1
+        total_votes = total_votes + 1
 
 # Print the following in the terminal
-# Print title "Election Analysis"
 print("Election Results")
 
-# Print "-----------------------" separator
 print("-----------------------------")
 
-# Print Total Votes Cast
-print(f'Total Votes Cast: {len(total_votes_cast)}')
+# Print Total Votes
+print(f'Total Votes: {(total_votes)}')
 
-# Write "-----------------------" separator
 print("-----------------------------")
 
-# Print out candidates with votes
-for item in candidates_unique: 
-    print(item) 
+# print Camdidate Names, % Votes Received, Total Votes Received
+for key in candidate_names.keys():
+    print(f'{key}: {float(candidate_names[key]/total_votes):.3%} ({candidate_names[key]})')
+    if candidate_names[key] > max_votes:
+        winner = key
+        max_votes = candidate_names[key]
 
-# Write Total Months, Total P/L, Average Change, Greatest Inc (date/amt), Greated Dec (data/amt)
-#print(f'Total Months: {len(count_months)}')
-#print(f'Total: ${int(sum(total_prof))}')
-#print(f'Total Monthly Change: ${int(sum(monthly_change))}')
-#print(f'Average Change: ${int(sum(monthly_change)/len(count_months))}')
-#print(f'Greatest Increase in Profits:  ${max(monthly_change)}')
-#print(f'Greatest Decrease in Profit: ${min(monthly_change)}')
-        
+print("-----------------------------")
 
-# Write the analysis results to a text file
+# print Winner
+print(f'Winner: {(winner)}')
+
+print("-----------------------------")
+
+
 # Set variable for the output file
-#pybank_output = os.path.join("Analysis", "pybank_analysis.txt")
+pypoll_output = os.path.join("Analysis", "pypoll_analysis.txt")
 
 # Open the output file
-#with open(pybank_output, "w") as datafile:
+with open(pypoll_output, "w") as datafile:
 
     # Write the following to the output text file
-    # Write title "Financial Analysis"
-    #datafile.write("Financial Analysis")
-
-    # Write "-----------------------" separator
-    #datafile.write("\n-----------------------------")
-
-    # Write Total Months, Total P/L, Average Change, Greatest Inc (date/amt), Greated Dec (data/amt)
-    #datafile.write(f'\nTotal Months: {len(count_months)}')
-    #datafile.write(f'\nTotal: ${int(sum(total_prof))}')
-    #datafile.write(f'\nAverage Change: ${int(sum(monthly_change)/len(monthly_change -1),2)}')
-    #datafile.write(f'\nGreatest Increase in Profits: ${max(monthly_change)}')
-    #datafile.write(f'\nGreatest Decrease in Profit: ${min(monthly_change)}')
-
-#Calcuate the average monthly change and round it    
-    #av_mon_chng = round(sum(changes)/(month_count -1),2)
+    # Write title "Election Results"
+    datafile.write("Election Results\n")
     
+    datafile.write("-----------------------------\n")
+   
+    # Write Total Votes
+    datafile.write(f'Total Votes: {(total_votes)}\n')
+    
+    datafile.write("-----------------------------\n")
+    
+    for key in candidate_names.keys():
+        datafile.write(f'{key}: {float(candidate_names[key]/total_votes):.3%} ({candidate_names[key]})\n')
+        if candidate_names[key] > max_votes:
+            winner = key
+        max_votes = candidate_names[key]
 
+    datafile.write("-----------------------------\n")
+
+    datafile.write(f'Winner: {(winner)}')
+
+    datafile.write("\n-----------------------------\n")
